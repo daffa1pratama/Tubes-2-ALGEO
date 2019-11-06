d7de17cd7729f6a2435974860badf536b6725843
fabianusharry <fabianusharry@yahoo.com>
2019-11-06 21:50:37 +0700

    gui

--- gui_mukegile.py
+++ gui_mukegile.py
@@ -34,6 +34,17 @@ def browse_button():
     filename = filedialog.askdirectory()
     folder_path.set(filename)
 
+def recognize():
+    jumlah = int(EntryJumlah.get())
+    inputselect = Metode.get()
+    if(inputselect == 1):
+        print("Metode yang anda pilih adalah Cosine Similarity")
+
+    elif(inputselect == 2):
+        print("Metode yang anda pilih adalah Euclidean Distance")
+    
+    print("Anda menginginkan " + str(jumlah) + " Hasil Wajah")
+
 window=Tk()
 window.geometry("750x750")
 window.title("Face Recognition Bray")
@@ -47,48 +58,50 @@ imglabel = Label(window, image=img).pack()
 
 
 
-button1=Button(window, text = "Cari Foto!", fg='black', bg ='blue', activebackground = "black", command=fileDialog , relief = RAISED, bd = 2, font=("arial",20,"bold"))
+button1=Button(window, text = "Cari Foto!", fg='black', bg ='blue', activebackground = "black", command=fileDialog , relief = RAISED, bd = 2, font=("arial",15,"bold"))
 button1.place(x=50, y=400)
-button2=Button(window, text = "Cari Folder!", fg='black', bg ='blue', relief = RAISED, command = browse_button, bd = 2, font=("arial",20,"bold"))
-button2.place(x=50, y=460)
-LabelRecognizer = Label(window, text = "Select Recognizer :", fg='blue', bg ='black' ,relief = 'solid', bd = 5, font=("arial",22,"bold"))
-LabelRecognizer.place(x=50, y= 510)
+button2=Button(window, text = "Cari Folder!", fg='black', bg ='blue', relief = RAISED, command = browse_button, bd = 2, font=("arial",15,"bold"))
+button2.place(x=50, y=440)
+button3=Button(window, text = "RECOGNIZE ME!", fg='blue', bg ='black', command = recognize, relief = RAISED, bd = 2, font=("arial",12))
+button3.place(x=200, y=605)
+LabelRecognizer = Label(window, text = "Select Recognizer :", fg='blue', bg ='black' ,relief = 'solid', bd = 5, font=("arial",15,"bold"))
+LabelRecognizer.place(x=50, y= 480)
 LabelHowMany = Label(window, text = "How Many Muke you want to cari brow?!?", fg='blue', bg ='black' ,relief = 'solid', bd = 5, font=("arial",10,"bold"))
-LabelHowMany.place(x = 50, y = 630)
+LabelHowMany.place(x = 50, y = 575)
 
-v = IntVar()
+Metode = IntVar()
 def ShowChoice():
-    print(v.get())
     global inputselect
-    inputselect = v.get()
+    inputselect = Metode.get()
 
 
 R1 = Radiobutton(window, 
               text="Cosine Similarity",
-              padx = 40, 
-              font=("arial",15,"italic"),
+              padx = 30, 
+              font=("arial",10,"italic"),
               activebackground = "black",
               activeforeground = "blue",
               bg = "black",
               fg = "blue",
-              indicator = 0,
-              variable=v, 
+              variable=Metode, 
               value=1)
-R1.place(x=50, y= 550)
+R1.place(x=50, y= 520)
 R2 = Radiobutton(window, 
               text="Euclidean Distance",
-              font=("arial",15,"italic"),
+              font=("arial",10,"italic"),
               activebackground = "black",
               activeforeground = "blue",
               bg = "black",
               fg = "blue",
-              indicator = 0,
-              padx = 40, 
-              variable=v, 
+              padx = 30, 
+              variable=Metode, 
               value=2)
-R2.place(x=50, y= 590)
-#button3=Button(window, text = "RECOGNIZE ME!", fg='blue', bg ='black', relief = RAISED, bd = 2, font=("arial",28,"bold"))
-#button3.place(x=50, y=580)
+R2.place(x=50, y= 550)
+
+EntryJumlah = Entry(window)
+EntryJumlah.place(x = 60, y = 610)
+
+
 
 window.mainloop()
  
